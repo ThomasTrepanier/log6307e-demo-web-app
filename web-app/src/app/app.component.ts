d0d613a25d2051f4d7324de13f40a538bc21f224
ThomasTrepanier <thomtrepanier@gmail.com>
2023-11-06 18:30:16 -0500

    v3.0.0

--- web-app/src/app/app.component.ts
+++ web-app/src/app/app.component.ts
@@ -9,13 +9,22 @@ import { User, max } from '@thomastrepanier/demo-library/public-api';
 export class AppComponent {
   title = 'web-app';
 
-  user: User = {
+  john: User = {
     age: 45,
     name: 'John v2.0.0',
+  };
+
+  alice: User = {
+    age: 50,
+    name: 'Alice',
   };
 
   constructor() {
     console.log('AppComponent constructor');
     console.log(max(1, 2));
+  }
+
+  get oldestUser(): number | undefined {
+    return max(this.john.age, this.alice.age);
   }
 }
